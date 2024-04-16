1791dca96453f1e650dc064c38c9ad589c166dca
vikas God <vikas.g@fortune4.in>
2024-04-16 13:49:12 +0530

    changes

--- src/app/api/getAllDestinations/route.js
+++ src/app/api/getAllDestinations/route.js
@@ -2,31 +2,32 @@ import { getAllDestinationsController } from "@/controllers/apisController";
 import { NextResponse } from "next/server";
 import fs from "fs";
 import { apiList } from "@/utils/constants";
+import { isDataStaleChecker } from "@/utils/common";
 
 export const GET = async (req) => {
-  //   const params = req.nextUrl.searchParams;
-  //   let textData = params.get("query");
   try {
     const destinationData = fs.readFileSync(
       `${apiList.fileUpload}/allDestination.json`,
       "utf-8"
     );
-    if (destinationData) {
-      let ArrOfObj = { data: JSON.parse(destinationData).data };
-      
-      return NextResponse.json(ArrOfObj, { status: 200 });
+    let arr = JSON.parse(destinationData)
+    if (destinationData && isDataStaleChecker(arr.updatedAt) === false) {
+      return NextResponse.json(arr, { status: 200 });
     } else {
       const destinations = await getAllDestinationsController();
       if (destinations.status) {
-        const maindata = JSON.stringify(destinations);
-        fs.writeFile(`public/json/allDestination.json`, maindata, (err) => {
+        // const maindata = JSON.stringify(destinations);
+        let arr = {
+          updatedAt: new Date(),
+          data: destinations.data
+        }
+        fs.writeFile(`public/json/allDestination.json`, JSON.stringify(arr), (err) => {
           if (err) {
             console.error("Error writing file:");
           } else {
             console.log("File written successfully");
           }
         });
-
         return NextResponse.json(destinations, { status: 200 });
       } else {
         throw destinations;
--- src/app/api/getDestinationDetailByProductId/route.js
+++ src/app/api/getDestinationDetailByProductId/route.js
@@ -1,10 +1,53 @@
 import { getProductdetailsByID } from "@/controllers/apisController";
 import { NextResponse } from "next/server";
+import fs from "fs";
+import { apiList } from "@/utils/constants";
+import { readFile } from "@/utils/common";
 
 export const GET = async (req) => {
     try {
-        let data = await getProductdetailsByID(req)
-        return NextResponse.json(data, { status: 200 })
+        const params = req.nextUrl.searchParams;
+        let id = params.get("query");
+        let jsonData = await readFile('getProductDetailsById.json')
+
+        // try {
+        //     const destinationData = fs.readFileSync(
+        //         `${apiList.fileUpload}/getProductDetailsById.json`,
+        //         "utf-8"
+        //     );
+        //     jsonData = JSON.parse(destinationData);
+        // } catch (error) {
+        //     console.error("Error reading JSON file:", error);
+        //     jsonData = [];
+        // }
+        if (Array.isArray(jsonData)) {
+            let arr = jsonData.find(v => v.productCode === id);
+            if (arr) {
+                let arrData = {
+                    data: arr,
+                    message: "product details JSON"
+                }
+                return NextResponse.json(arrData, { status: 200 });
+            } else {
+                let data = await getProductdetailsByID(req);
+                jsonData.push({ ...data });
+                fs.writeFile(`${apiList.fileUpload}/getProductDetailsById.json`, JSON.stringify(jsonData), (err) => {
+                    if (err) {
+                        console.error("Error writing file:", err);
+                    } else {
+                        console.log("File written successfully");
+                    }
+                });
+                let arrData = {
+                    data: data,
+                    message: "Fetch product details successfully"
+                }
+                return NextResponse.json(arrData, { status: 200 });
+            }
+        } else {
+            console.error("Data is not an array:", jsonData);
+            return NextResponse.json({ error: "Data is not an array" }, { status: 500 });
+        }
     } catch (err) {
         console.log("err", err);
         return NextResponse.json(err, { status: 500 });
