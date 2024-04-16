fbeec3b182fd6df343fd3bac7d998dc0ba8be6de
Suraj Sangale <suraj.sangale@fortune4.in>
2024-04-16 14:45:38 +0530

    logic chnage json

--- src/app/api/getDestinationById/route.js
+++ src/app/api/getDestinationById/route.js
@@ -6,48 +6,58 @@ import { apiList } from "@/utils/constants";
 export const POST = async (req) => {
   try {
     const requestBody = await req.json();
-    const destinationByIdData = fs.readFileSync(
-      `${apiList.fileUpload}/destinationById.json`,
-      "utf-8"
-    );
-    let readArr = destinationByIdData && JSON.parse(destinationByIdData);
-    let check =
-      readArr.length > 0 &&
-      readArr.filter(
-        (item) => item.destinationId === requestBody?.filtering?.destination
+    let jsonData;
+    try {
+      const destinationByIdData = fs.readFileSync(
+        `${apiList.fileUpload}/destinationById.json`,
+        "utf-8"
       );
-    if (Object.keys(check).length === 1) {
-      console.log("in json destination by id");
-      let ArrOfObj = check[0]?.destinationData;
+      jsonData = JSON.parse(destinationByIdData);
+    } catch (error) {
+      jsonData = [];
+    }
+    if (Array.isArray(jsonData)) {
+      let arr = jsonData.find(
+        (v) => v.destinationId === requestBody?.filtering?.destination
+      );
+      if (arr) {
+        // console.log("in json destination by id");
+        let ArrOfObj = arr.destinationData;
 
-      return NextResponse.json(ArrOfObj, { status: 200 });
-    } else {
-      console.log("in Api destination by id");
-      const destinationById = await getDestinationByIdController(requestBody);
-
-      const singleData = {
-        destinationId: requestBody?.filtering?.destination,
-        destinationData: destinationById,
-      };
-      readArr.length > 0 ? readArr.push(singleData) : (readArr = [singleData]);
-      const jsonData = JSON.stringify(readArr);
-
-      if (destinationById.status) {
-        fs.writeFile(
-          `${apiList.fileUpload}/destinationById.json`,
-          jsonData,
-          (err) => {
-            if (err) {
-              console.error("Error writing file:");
-            } else {
-              console.log("File written successfully");
-            }
-          }
-        );
-        return NextResponse.json(destinationById, { status: 200 });
+        return NextResponse.json(ArrOfObj, { status: 200 });
       } else {
-        throw destinationById;
+        // console.log("in Api destination by id");
+        const destinationById = await getDestinationByIdController(requestBody);
+
+        const singleData = {
+          destinationId: requestBody?.filtering?.destination,
+          destinationData: destinationById,
+        };
+
+        jsonData.push({ ...singleData });
+        if (destinationById.status) {
+          fs.writeFile(
+            `${apiList.fileUpload}/destinationById.json`,
+            JSON.stringify(jsonData),
+            (err) => {
+              if (err) {
+                console.error("Error writing file:");
+              } else {
+                console.log("File written successfully");
+              }
+            }
+          );
+          return NextResponse.json(destinationById, { status: 200 });
+        } else {
+          throw destinationById;
+        }
       }
+    } else {
+      console.error("Data is not an array:", jsonData);
+      return NextResponse.json(
+        { error: "Data is not an array" },
+        { status: 500 }
+      );
     }
   } catch (error) {
     console.error("getDestinationById Api error", error);
